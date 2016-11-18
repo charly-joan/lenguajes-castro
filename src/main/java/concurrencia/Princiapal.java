bb2ef4fd4cc6545b0c373a6153c76d5f4e58efa3
T-107 <T-107@PC250>
2016-11-17 19:11:07 -0600

    mi primer pushero

---
+++ src/main/java/concurrencia/Princiapal.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concurrencia;
+
+/**
+ *
+ * @author T-107
+ */
+public class Princiapal implements Runnable{
+    /*
+ segunda forma de crear threads implementando la interface runnable
+    usando polimorfiasmo
+    */
+    public static void main(String[] args) {
+        
+        Runnable r=new Princiapal();
+        Thread t1=new Thread(r);
+        t1.setName("juan");
+        Thread t2=new Thread(r);
+       t2.setName("ana");
+        //t1.start();
+        //t2.start();
+        
+        
+        //la tercera forma que viene en el examen 
+        //usando clases internas anonimas
+        
+        
+        Thread t3=new Thread(new Runnable(){
+            @Override
+            public void run() {
+                
+                System.out.println("soy un background thread");
+            }
+            
+        });
+    }
+
+    @Override
+    public void run() {
+       /*
+        System.out.println("soy un thread y me llamo"+Thread.currentThread().getName());
+        
+        if(Thread.currentThread().getName().equals("juan")){
+            System.out.println("soy hombre");
+        }
+        else
+            System.out.println("soy mujer");
+
+*/
+       
+       
+    }
+    
+}
